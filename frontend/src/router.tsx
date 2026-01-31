b17b59109251936c1a3abca0df2cfe7a11c38cc2
yjz94123 <zyj94123@gmail.com>
2026-01-31 14:25:50 +0800

    更新

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
 import MarketList from './pages/MarketList';
-import MarketDetail from './pages/MarketDetail';
+import { MarketDetailPage } from './pages/MarketDetailPage';
 import Portfolio from './pages/Portfolio';
 
 export const AppRouter = () => {
@@ -9,7 +9,7 @@ export const AppRouter = () => {
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<MarketList />} />
-        <Route path="market/:marketId" element={<MarketDetail />} />
+        <Route path="market/:marketId" element={<MarketDetailPage />} />
         <Route path="portfolio" element={<Portfolio />} />
       </Route>
     </Routes>
